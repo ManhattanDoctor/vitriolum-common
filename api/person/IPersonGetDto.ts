b6ba49560b89eb47b516950556d513e7e7d6652d
Renat Gubaev <gubaev.r@n-t.io>
2023-10-18 14:39:32 +0300

    no message

--- api/person/IPersonGetDto.ts
+++ api/person/IPersonGetDto.ts
@@ -3,7 +3,6 @@ import { ITraceable } from '@ts-core/common';
 import { Person } from '../../lib/person';
 
 export interface IPersonGetDto extends ITraceable {
-    uid: string;
     manner?: string;
 }
 export declare type IPersonGetDtoResponse = Person;

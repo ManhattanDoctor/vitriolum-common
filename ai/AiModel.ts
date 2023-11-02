4986f58f228616512d8b780ec8136bc8bb50fb97
Renat Gubaev <gubaev.r@n-t.io>
2023-11-02 22:17:53 +0300

    no message

--- ai/AiModel.ts
+++ ai/AiModel.ts
@@ -1,5 +1,7 @@
 export enum AiTextModel {
-    GPT_35_TURBO_16K = 'GPT_35_TURBO_16K'
+    GPT_4 = 'gpt-4',
+    GPT_4_32K = 'gpt-4-32k',
+    GPT_35_TURBO = 'gpt-3.5-turbo'
 }
 export enum AiImageModel {
     STABLE_DIFFUSION = 'STABLE_DIFFUSION',

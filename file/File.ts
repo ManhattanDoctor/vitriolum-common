04ed26295038af90ccb1fb110639d486defed8d2
Renat Gubaev <gubaev.r@n-t.io>
2023-12-19 15:37:31 +0300

    no message

--- file/File.ts
+++ file/File.ts
@@ -5,7 +5,7 @@ import * as _ from 'lodash';
 export class File {
     id: number;
     uid: string;
-    type: string;
+    type: FileType;
     name: string;
     path: string;
     size: number;
@@ -22,16 +22,38 @@ export class File {
     createdDate: Date;
 }
 
-export const FilePictureExtensions = ['jpg', 'jpeg', 'png'];
-export const FileDocumentExtensions = ['pdf', 'xlsx', 'xls', 'docx', 'doc'];
-export const FileExtensions = [...FilePictureExtensions, ...FileDocumentExtensions];
+export enum FileImageExtension {
+    PNG = 'png',
+    JPG = 'jpg',
+    JPEG = 'jpeg',
+}
+export enum FileDocumentExtension {
+    PDF = 'png',
+    XLS = 'xls',
+    DOC = 'doc',
+    DOCX = 'docx',
+    XLSX = 'xlsx',
+}
+export const FileImageExtensions: Array<string> = Object.values(FileImageExtension);
+export const FileDocumentExtensions: Array<string> = Object.values(FileDocumentExtension);
+
+export const FileExtensions = [...FileImageExtensions, ...FileDocumentExtensions];
 
 export const FileMimes = [
-    'image/jpeg',
     'image/png',
+    'image/jpeg',
     'application/pdf',
     'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
     'application/vnd.ms-excel',
     'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
     'application/msword'
 ];
+
+export enum FileType {
+    IMAGE = 'IMAGE',
+    DOCUMENT = 'DOCUMENT',
+}
+
+export const FILE_SIZE_MAX = 5242880; // 5mb
+export const FILE_NAME_MAX_LENGTH = 124;
+export const FILE_AMOUNT_MAX = 10;

e888d44912ab4be0a0a1239f61d2919d9185bc76
Renat Gubaev <gubaev.r@n-t.io>
2024-01-18 12:40:10 +0300

    no message

--- api/openai/IOpenAiAgentAddDto.ts
+++ api/openai/IOpenAiAgentAddDto.ts
@@ -2,11 +2,13 @@
 import { ITraceable } from '@ts-core/common';
 import { OpenAiAgent, OpenAiAgentTool } from '../../ai/model/openai';
 import { OpenAiTextModel } from '../../ai/model/openai';
+import { Color } from '../../color';
 
 export interface IOpenAiAgentAddDto extends ITraceable {
     name: string;
     model: OpenAiTextModel;
 
+    color?: Color;
     tools?: Array<OpenAiAgentTool>;
     files?: Array<number>;
     system?: string;

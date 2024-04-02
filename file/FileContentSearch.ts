65dd4547c6e108e72ae58d6fc02db00ead826019
Renat Gubaev <gubaev.r@n-t.io>
2024-04-02 15:18:36 +0300

    no message

--- file/FileContentSearch.ts
+++ file/FileContentSearch.ts
@@ -2,3 +2,8 @@ export interface IFileContentSearchOptions {
     maxDocuments: number;
     scoreThreshold: number;
 }
+
+export const FILE_CONTENT_SEARCH_MAX_DOCUMENTS_MIN = 1;
+export const FILE_CONTENT_SEARCH_MAX_DOCUMENTS_MAX = 10;
+export const FILE_CONTENT_SCORE_THRESHOLD_MIN = 0;
+export const FILE_CONTENT_SCORE_THRESHOLD_MAX = 10;

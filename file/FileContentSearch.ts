d618de42d82411dd697ecb1555818330cd2224e5
Renat Gubaev <gubaev.r@n-t.io>
2024-05-06 13:44:25 +0300

    no message

--- file/FileContentSearch.ts
+++ file/FileContentSearch.ts
@@ -1,8 +1,19 @@
+import { File } from "./File";
+
 export interface IFileContentSearchOptions {
-    files?: Array<number>;
+    scope?: FileContentSearchScope;
+    files?: Array<FileSearchInfo>;
     minScore: number;
     maxDocuments: number;
 }
+
+export enum FileContentSearchScope {
+    ANY = 'ANY',
+    BASE_ONLY = 'BASE_ONLY',
+    FILES_ONLY = 'FILES_ONLY',
+}
+
+export type FileSearchInfo = number | File;
 
 export const FILE_CONTENT_SEARCH_MAX_DOCUMENTS_MIN = 1;
 export const FILE_CONTENT_SEARCH_MAX_DOCUMENTS_MAX = 10;

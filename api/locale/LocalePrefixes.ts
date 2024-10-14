37afe0d54a913dfd4e814c8cc70ce4e820f76b07
Renat Gubaev <gubaev.r@n-t.io>
2024-10-14 17:25:23 +0300

    no message

--- api/locale/LocalePrefixes.ts
+++ api/locale/LocalePrefixes.ts
@@ -1,20 +1,52 @@
 
 export enum LocaleProject {
-    GNOSISAI = 'gnosisai'
+    GNOSISAI = 'gnosisai',
+    GNOSISAI_LITE = 'gnosisai-lite',
+    GNOSISAI_SACCUS = 'gnosisai-saccus',
 }
+
+export const GNOSISAI_LOCALES = ['en', 'ru'];
 
 export const GNOSISAI_LOCALE_PREFIXES = [
     '.json',
     'Custom.json',
 
-    'User.json',
-
     'Ai.json',
+    'User.json',
     'Coin.json',
     'File.json',
-    'Main.json',
     'About.json',
     'Payment.json',
     'Management.json',
     'Conversation.json',
+
+    'Gnosis.json',
+];
+
+export const GNOSISAI_LITE_LOCALE_PREFIXES = [
+    '.json',
+    'Custom.json',
+
+    'Ai.json',
+    'User.json',
+    'File.json',
+    'Main.json',
+    'About.json',
+    'Conversation.json',
+
+    'Lite.json'
+];
+
+export const GNOSISAI_SACCUS_LOCALE_PREFIXES = [
+    '.json',
+    'Custom.json',
+
+    'Ai.json',
+    'User.json',
+    'File.json',
+    'Main.json',
+    'About.json',
+    'Conversation.json',
+
+    'Saccus.json'
 ];

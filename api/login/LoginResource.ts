8ff0c7c2e1c7f4f4ea1b97237c7430c7fd11b64e
Renat Gubaev <gubaev.r@n-t.io>
2024-10-15 15:42:03 +0300

    no message

--- api/login/LoginResource.ts
+++ api/login/LoginResource.ts
@@ -1,8 +1,11 @@
 export enum LoginResource {
+    VK_INTERNAL = 'VK_INTERNAL',
+    TELEGRAM_INTERNAL = 'TELEGRAM_INTERNAL',
+
     VK = 'VK',
     MAIL = 'MAIL',
     YANDEX = 'YANDEX',
     GOOGLE = 'GOOGLE',
+    ETHEREUM = 'ETHEREUM',
     TELEGRAM = 'TELEGRAM',
-    ETHEREUM = 'ETHEREUM'
 }

519a5eb0154b6a466518927a2ebe971d2e636f77
Renat Gubaev <gubaev.r@n-t.io>
2024-10-23 17:08:49 +0300

    no message

--- file/FileContentSearch.ts
+++ file/FileContentSearch.ts
@@ -19,7 +19,9 @@ export enum FileContentSearchScope {
 
 export type FileSearchInfo = number | File;
 
-export const FILE_CONTENT_SEARCH_MAX_DOCUMENTS_MIN = 1;
-export const FILE_CONTENT_SEARCH_MAX_DOCUMENTS_MAX = 1_000_000;
 export const FILE_CONTENT_MIN_SCORE_MIN = 0;
 export const FILE_CONTENT_MIN_SCORE_MAX = 1;
+export const FILE_CONTENT_SEARCH_DIVIDER_MIN_LENGTH = 1;
+export const FILE_CONTENT_SEARCH_DIVIDER_MAX_LENGTH = 16;
+export const FILE_CONTENT_SEARCH_MAX_DOCUMENTS_MIN = 1;
+export const FILE_CONTENT_SEARCH_MAX_DOCUMENTS_MAX = 1_000_000;

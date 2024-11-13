096b973cf7857e8887bead3de62cdae2c07e25bb
Renat Gubaev <gubaev.r@n-t.io>
2024-11-13 18:30:03 +0300

    no message

--- util/ShareUtil.ts
+++ util/ShareUtil.ts
@@ -11,6 +11,7 @@ export class ShareUtil {
     public static VK_URL = 'https://vk.com/gnosisai';
     public static SITE_URL = 'https://gnosisai.ru';
     public static ASSETS_URL = 'https://vk.occultist.one/gnosisai/assets';
+    public static SACCUS_SITE_URL = 'https://saccus.gnosisai.ru';
 }
 
 export interface IShareUrl {

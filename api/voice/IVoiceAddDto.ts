36b535d0e6fda8f3c2fc8c8e383e5ed075e35c5f
Renat Gubaev <gubaev.r@n-t.io>
2024-12-04 13:14:24 +0300

    no message

--- api/voice/IVoiceAddDto.ts
+++ api/voice/IVoiceAddDto.ts
@@ -1,9 +1,10 @@
 import { ITraceable } from '@ts-core/common';
-import { Voice } from '../../voice';
+import { Voice, VoiceResource } from '../../voice';
 
 export interface IVoiceAddDto extends ITraceable {
     name: string;
     fileId: number;
+    resource: VoiceResource;
 }
 
 export type IVoiceAddDtoResponse = Voice;

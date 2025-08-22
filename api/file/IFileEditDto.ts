c897e8f1e44d39f68836c072e460e47d4ca4adda
Renat Gubaev <gubaev.r@n-t.io>
2025-08-22 13:38:35 +0300

    no message

--- api/file/IFileEditDto.ts
+++ api/file/IFileEditDto.ts
@@ -3,6 +3,7 @@ import { File } from '../../file';
 export interface IFileEditDto extends Partial<File> {
     id: number;
     name?: string;
+    tags?: Array<string>;
 }
 
 export type IFileEditDtoResponse = File;
